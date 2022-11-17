1612b3ea6f719e9ae47df09c90caf7f723991c35
paquijanoc <111616862+paquijanoc@users.noreply.github.com>
2022-11-17 17:25:33 -0500

    P200

--- preguntas.py
+++ preguntas.py
@@ -7,7 +7,6 @@ En este laboratio se construirá un clasificador usando k-NN para el dataset de 
 """
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 from sklearn import datasets
 
 
@@ -15,7 +14,7 @@ def pregunta_01():
     """
     Complete el código presentado a continuación.
     """
-    import pandas as pd
+    
     # Cargue el dataset digits
     digits = datasets.load_digits()
 
@@ -66,7 +65,7 @@ def pregunta_03():
     """
     Complete el código presentado a continuación.
     """
-
+    import pandas as pd
     # Importe KNeighborsClassifier de sklearn.neighbors
     from sklearn.neighbors import KNeighborsClassifier
 
