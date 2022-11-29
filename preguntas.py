2bdc63e1333bb7dc0f6153814506c289ae807712
paquijanoc <111616862+paquijanoc@users.noreply.github.com>
2022-11-29 11:28:01 -0500

    Intento1

--- preguntas.py
+++ preguntas.py
@@ -7,6 +7,7 @@ En este laboratio se construirá un clasificador usando k-NN para el dataset de 
 """
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from sklearn import datasets
 
 
